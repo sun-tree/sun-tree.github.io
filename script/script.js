8b05af83c8c90a1e2f96b0c2410e9a44e74d3cc1
Tokyodays <kikuchi@gmail.com>
2016-03-20 23:41:32 +0900

    speed up

--- script/script.js
+++ script/script.js
@@ -1,9 +1,24 @@
 $(function(){
+  var _css_array = [
+    "CSS_GOOGLE",
+    "CSS_BOOTSTRAP",
+    "CSS_FULLPAGE",
+    "CSS_FONT",
+    "CSS_MAIN"
+  ]
+
+  for(var i = 0, ileng = _css_array.length; i < ileng; i++){
+    var n = document.getElementById(_css_array[i]);
+    n.rel = 'stylesheet';
+  }
+
   $('#fullpage').fullpage({
     sectionsColor: ['#ffffff', '#ffffff', '#ffffff', '#ffffff'],
     anchors: ['home', 'about', 'works', 'contact'],
     menu: '#menu',
     continuousVertical: false,
+    navigation: true,
+    navigationPosition: 'right',
     navigationTooltips: ['Home', 'About', 'works', 'Contact']
   });
 })
