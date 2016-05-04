1bae635e5a28d61c83df2909be5dc76c4c211a6f
Tokyodays <kikuchi@gmail.com>
2016-05-04 12:04:42 +0200

    renewal

--- script/script.js
+++ script/script.js
@@ -1,11 +1,3 @@
 $(function(){
-  $('#fullpage').fullpage({
-    sectionsColor: ['#ffffff', '#ffffff', '#ffffff'],
-    anchors: ['home', 'about', 'contact'],
-    menu: '#menu',
-    continuousVertical: false,
-    navigation: true,
-    navigationPosition: 'right',
-    navigationTooltips: ['Home', 'About', 'Contact']
-  });
+
 })
